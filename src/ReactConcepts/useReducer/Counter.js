21f3228969826dab84993e258077614a0f2b4a54
Divine <dpkreativ@gmail.com>
2022-02-22 09:37:07 +0100

    counter implemented with useReducer

--- src/ReactConcepts/useReducer/Counter.js
+++ src/ReactConcepts/useReducer/Counter.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const initialState = 0;
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'add':
+        return state + action.payload;
+      case 'subtract':
+        return state - action.payload;
+      case 'reset':
+        return initialState;
+      default:
+        return new Error();
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <h3>Counter</h3>
+      <h1>{state}</h1>
+
+      <button onClick={() => dispatch({ type: 'subtract', payload: 1 })}>
+        Decrease
+      </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+      <button onClick={() => dispatch({ type: 'add', payload: 2 })}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
