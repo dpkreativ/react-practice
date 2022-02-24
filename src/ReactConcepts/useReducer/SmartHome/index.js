2f8af563b3dc4c857cb939e97db4d63f6abc184d
Divine <dpkreativ@gmail.com>
2022-02-24 06:48:16 +0100

    render state data on UI

--- src/ReactConcepts/useReducer/SmartHome/index.js
+++ src/ReactConcepts/useReducer/SmartHome/index.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import './SmartHome.css';
 
 const SmartHome = () => {
-  return <div>SmartHome</div>;
+  const appliances = [
+    {
+      name: 'bulbs',
+      active: false,
+    },
+    {
+      name: 'air conditioner',
+      active: true,
+    },
+    {
+      name: 'music',
+      active: true,
+    },
+    {
+      name: 'television',
+      active: false,
+    },
+  ];
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'deactivate':
+        return state.map((appliance) => {
+          if (appliance.name === action.payload) {
+            appliance.active = false;
+          }
+          return appliance;
+        });
+      case 'activate':
+        return state.map((appliance) => {
+          if (appliance.name === action.payload) {
+            appliance.active = true;
+          }
+          return appliance;
+        });
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, appliances);
+
+  return (
+    <div className="container">
+      <h1>SmartHome</h1>
+      <div className="grid">
+        {state.map((appliance, idx) => (
+          <div key={idx} className="card">
+            <h2>{appliance.name}</h2>
+            {appliance.active ? (
+              <div className="status active">Active</div>
+            ) : (
+              <div className="status inactive">Not active</div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SmartHome;
